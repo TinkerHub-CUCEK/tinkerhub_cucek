7c4d9d0efc42317c015684b32986444e53750529
bhagyaj <bhagyajkumar1@gmail.com>
2023-05-23 17:18:49 +0530

    updated with new team

--- src/Components/TeamCards.jsx
+++ src/Components/TeamCards.jsx
@@ -1,59 +1,10 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
-import arjun from "../assets/team/arjun.jpeg"
-import anu from "../assets/team/anu.jpeg"
-import riya from "../assets/team/riya.jpg"
-import dhilshad from "../assets/team/dhilshad.jpg"
-import dinasree from "../assets/team/dinasree.jpg"
-import ashika from "../assets/team/ashika.jpg"
-import stephy from "../assets/team/stephy.jpeg"
-import abhijith from "../assets/team/abhijith.jpg"
+import teamMembers from '../data/team';
 
 function TeamCards() {
-    const teamMembers = [
-        {
-            "name": "Arjun",
-            "photo": arjun,
-            "title": "Campus Lead"
-        },
-        {
-            "name": "Abhijith Ram P",
-            "photo": abhijith,
-            "title": "Tech Lead"
-        },
-        {
-            "name": "Ashika",
-            "photo": ashika,
-            "title": "WIT Lead"
-        },
-        {
-            "name": "Anu fasmin",
-            "photo": anu,
-            "title": "WIT Co-lead"
-        },
-        {
-            "name": "Riya Rose James",
-            "photo": riya,
-            "title": 'WIT Creative lead'
-        },
-        {
-            "name": "Dhilshad",
-            "photo": dhilshad,
-            "title": 'Media Lead'
-        },
-        {
-            "name": "Dinasree",
-            "photo": dinasree,
-            "title": "WIT Media Lead"
-        },
-        {
-            "name": "Stephy",
-            "photo": stephy,
-            "title": "WIT Tech Lead"
-        }
-    ]
+    
     return (
         <Row xs={1} md={3} className="g-4">
             {
---
+++ src/data/team.js
@@ -0,0 +1,41 @@
+import Ayisha from "../assets/team/ayisha.png"
+import Bhagyaj from "../assets/team/bhagyaj.png"
+import Treesa from "../assets/team/treesa.png"
+import Govind from "../assets/team/govind.png"
+import Arnav from "../assets/team/arnav.png"
+import Aiswarya from "../assets/team/aiswarya.png"
+
+
+const teamMembers = [
+    {
+        "name": "Ayisha Hanna",
+        "photo": Ayisha,
+        "title": "Campus Lead"
+    },
+    {
+        "name": "Bhagyaj Kumar",
+        "photo": Bhagyaj,
+        "title": "Tech Lead"
+    },
+    {
+        "name": "Treesa George",
+        "photo": Treesa,
+        "title": "WIT Lead"
+    },
+    {
+        "name": "Govind R Nair",
+        "photo": Govind,
+        "title": "Program Facilitator"
+    },
+    {
+        "name": "Arnav Kashyap",
+        "photo": Arnav,
+        "title": "Program Facilitator",
+    },
+    {
+        "name": "Aiswarya",
+        "photo": Aiswarya
+    }
+]
+
+export default teamMembers
